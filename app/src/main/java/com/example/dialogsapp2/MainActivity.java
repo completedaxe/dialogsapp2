d51088c6f3b9b635d4bee9ede32d98b824d049ce
completedaxe <49436206+completedaxe@users.noreply.github.com>
2020-10-28 16:45:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/dialogsapp2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dialogsapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+
+        ListView phonesList = (ListView) findViewById(R.id.phonesList);
+        ArrayList<String> phones = new ArrayList<>();
+        phones.add("Google Pixel");
+        phones.add("Huawei P9");
+        phones.add("LG G5");
+        phones.add("Samsung Galaxy S8");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phones);
+        phonesList.setAdapter(adapter);
+
+        phonesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selectedPhone = adapter.getItem(position);
+                CustomDialogFragment dialog = new CustomDialogFragment();
+                Bundle args = new Bundle();
+                args.putString("phone", selectedPhone);
+                dialog.setArguments(args);
+            }
+        });
+    }
+}
